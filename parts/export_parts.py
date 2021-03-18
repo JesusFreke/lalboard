6d8084272c95b424b62c9ded3c81c1a11f10c692
Ben Gruver <bgruv@google.com>
2021-03-17 22:23:13 -0700

    Add support for generating various exports for assemblies in export_parts.py
    This will export an assembly as an stl, an f3d, and to a specific
    fusion 360 cloud instance (which can then be shared via link, for viewing
    on the web by anyone)

--- parts/export_parts.py
+++ parts/export_parts.py
@@ -33,13 +33,16 @@ def run(_):
         file: os.DirEntry
         for file in os.scandir(os.path.dirname(__file__)):
             if file.is_dir():
-                if (file.name.startswith("_") or file.name.endswith("assembly") or file.name == "__pycache__" or
+                if (file.name.startswith("_") or file.name == "__pycache__" or
                         file.name == "scene"):
                     continue
 
                 export_sketch = False
+                export_assembly = False
                 if file.name.endswith("sketch"):
                     export_sketch = True
+                if file.name.endswith("assembly"):
+                    export_assembly = True
 
                 path = pathlib.Path(file.path, file.name + ".py")
 
@@ -52,16 +55,12 @@ def run(_):
                         raise Exception("Unexpected number of sketches in design")
                     sketch: adsk.fusion.Sketch = root().sketches[0]
                     sketch.saveAsDXF(str(pathlib.Path(export_dir, file.name + ".dxf")))
+                elif export_assembly:
+                    export_stl(export_dir, file)
+                    f3d_file =export_f3d(export_dir, file)
+                    export_to_fusion_cloud(file, f3d_file)
                 else:
-                    if len(root().occurrences) != 1:
-                        raise Exception("Unexpected number of components in design")
-                    options = design().exportManager.createSTLExportOptions(
-                        root().occurrences[0],
-                        str(pathlib.Path(export_dir, file.name + ".stl")))
-                    options.meshRefinement = adsk.fusion.MeshRefinementSettings.MeshRefinementHigh
-                    options.sendToPrintUtility = False
-
-                    design().exportManager.execute(options)
+                    export_stl(export_dir, file)
 
                 app().activeDocument.close(saveChanges=False)
 
@@ -70,3 +69,70 @@ def run(_):
     except Exception:
         print(traceback.format_exc())
         ui().messageBox('Failed:\n{}'.format(traceback.format_exc()))
+
+
+def export_stl(export_dir, file):
+    options = design().exportManager.createSTLExportOptions(
+        root(),
+        str(pathlib.Path(export_dir, file.name + ".stl")))
+    options.meshRefinement = adsk.fusion.MeshRefinementSettings.MeshRefinementHigh
+    options.sendToPrintUtility = False
+
+    design().exportManager.execute(options)
+
+
+def export_f3d(export_dir, file):
+    options = design().exportManager.createFusionArchiveExportOptions(
+        str(pathlib.Path(export_dir, file.name + ".f3d")),
+        root())
+    design().exportManager.execute(options)
+    return str(pathlib.Path(export_dir, file.name + ".f3d"))
+
+
+printed_export_to_fusion_cloud_warning = False
+
+
+def export_to_fusion_cloud(file, f3d_file):
+    # hardcoded id of my "lalboard exports" project
+    dataProject = app().data.dataProjects.itemById("a.cGVyc29uYWw6dWUyZDJjMmQxIzIwMjEwMzE4Mzk0MTc4ODAx")
+
+    if dataProject is None:
+        global printed_export_to_fusion_cloud_warning
+        if not printed_export_to_fusion_cloud_warning:
+            print("Could not find the correct fusion 360 project. You probably need to create a project and change "
+                  "the hardcoded id in export_to_fusion_cloud in the export_parts.py script")
+            printed_export_to_fusion_cloud_warning = True
+            return
+
+    data_file_for_export: adsk.core.DataFile = None
+    for data_file in dataProject.rootFolder.dataFiles:
+        if data_file and data_file.name == file.name:
+            data_file_for_export = data_file
+            break
+    if not data_file_for_export:
+        # first, create the new document as an empty document. We rely on the fact that the first version is empty.
+        if not app().activeDocument.saveAs(
+                file.name,
+                dataProject.rootFolder,
+                "",
+                ""):
+            raise Exception("Oops, couldn't save document")
+        return
+
+    document = app().documents.open(data_file_for_export)
+    design: adsk.fusion.Design = document.products[0]
+
+    occurrence: adsk.fusion.Occurrence
+    for occurrence in design.rootComponent.occurrences:
+        occurrence.deleteMe()
+
+    imported_occurrence: adsk.fusion.Occurrence = app().importManager.importToTarget2(
+        app().importManager.createFusionArchiveImportOptions(f3d_file),
+        design.rootComponent)[0]
+
+    for occurrence in imported_occurrence.component.occurrences:
+        occurrence.moveToComponent(design.rootComponent.allOccurrences[0])
+
+    imported_occurrence.deleteMe()
+    document.save("saved")
+    document.close(saveChanges=False)
